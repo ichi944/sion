740d911ec3e21a42f65decff4f445b90244c2d0b
ichi944 <ichi944g@gmai..com>
2021-09-04 17:13:54 +0900

    Feat: add schema and migration for story point

---
+++ server/prisma/migrations/20210904081020_add_story_point/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "StoryPoint" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "epicId" INTEGER NOT NULL,
+    "point" REAL NOT NULL,
+    FOREIGN KEY ("epicId") REFERENCES "Epic" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StoryPoint_epicId_unique" ON "StoryPoint"("epicId");
